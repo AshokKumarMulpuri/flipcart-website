f6e65f118a6cebd91b1b6b08b04f8d6c02ade168
Ashok Kumar Mulpuri <ashokkumar.m2043@gmail.com>
2024-07-29 10:41:48 -0500

    action update

--- src/Components/Redux/action.js
+++ src/Components/Redux/action.js
@@ -1,18 +1,5 @@
-// export const ADD_TO_CART = "ADD_TO_CART";
-// export const SET_INVENTORY = "SET_INVENTORY";
-
-// export const addToCart = (item) => ({
-//   type: ADD_TO_CART,
-//   item,
-// });
-
-// export const setInventory = (inventory) => ({
-//   type: SET_INVENTORY,
-//   inventory,
-// });
 export const ADD_TO_CART = "ADD_TO_CART";
 export const SET_INVENTORY = "SET_INVENTORY";
-export const UPDATE_CART_QUANTITY = "UPDATE_CART_QUANTITY";
 
 export const addToCart = (item) => ({
   type: ADD_TO_CART,
@@ -22,10 +9,4 @@ export const addToCart = (item) => ({
 export const setInventory = (inventory) => ({
   type: SET_INVENTORY,
   inventory,
-});
-
-export const updateCartQuantity = (item, quantity) => ({
-  type: UPDATE_CART_QUANTITY,
-  item,
-  quantity,
 });
